c65bd2352a437e0585264199673f223e822fc9e7
Kevin Lapi <kevinlapi@hotmail.com>
2024-03-14 05:06:13 +0100

    careers aggiornato

--- resources/views/article/careers.blade.php
+++ resources/views/article/careers.blade.php
@@ -7,42 +7,20 @@
                     Lavora con noi
                 </h1>
             </div>
-            <div class="container-fluid h-100 p-0" style="">
-                <div class="row h-100 justify-content-center align-items-center">
-<<<<<<< HEAD
+      
+
 
                     <div class="col-lg-4 col-md-6 col-sm-8 m-3">
                         <img src="/images/image-careers.png" alt="Descrizione dell'immagine" class="img-fluid"
-=======
-                    <div class="col-lg-4 col-md-6 col-sm-8 m-3">
-                        <div>
-                            <label for="tags"
-                                class="form-label"style="font-size: 22px;font-family: 'Bebas Neue', cursive; ">Tags</label>
-                            <input name="tags" id="tags" class="form-control" value="{{ old('tags') }}"
-                                style="max-width: 500px;">
-                            <span class="" style="font-size: 36px;font-family: 'Bebas Neue', cursive; ">Dividi
-                                ogni tag con una virgola</span>
-                        </div>
 
-                        <div>
-                            <button class="mt-4  button-glitch2 align-items-center justify-content-center">Inserisci un
-                                articolo</button>
-                            <a class="mt-4  button-glitch2 align-items-center justify-content-center ml-4"
-                                href="{{ route('homepage') }}">Torna alla home</a>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-8 m-3">
-                        <img src="/images/tags-create.png" alt="Descrizione dell'immagine" class="img-fluid"
->>>>>>> 908cae85dbfe4b9fdabd735fc95a4d4272bc770a
-                            style="max-width: 600px; max-height: 600px;">
                     </div>
                 </div>
-            </div>
+         
             <div class="d-flex p-3 h50 main-div justify-content-end">
                 <div class="col-lg-5 col-md-12 d-flex px-5">
                     <div class="welcome-text justify-content-start" style="padding-left: 20px;">
                         <span class="small-text"
-<<<<<<< HEAD
+
                             style="font-family:'Roboto', sans-serif; font-size: 1vw; letter-spacing: 1px;">cosa
                             </span>
                         <span class="large-text mx-3"
@@ -70,39 +48,8 @@
                             attraverso ogni articolo.</p>
                     </div>
                 </div>
-=======
-                            style="font-family:'Roboto', sans-serif; font-size: 15px; letter-spacing: 1px;">perchè
-                            abbiamo</span>
-                        <span class="large-text mx-3"
-                            style="font-family: 'Roboto', sans-serif; font-size: 24px; letter-spacing: 1px;">bisogno di
-                            te</span>
-
-                    </div>
-                </div>
-            </div>
-            <div class="d-flex justify-content-between bg-white" style="padding: 20px;">
-                <div class="col-lg-7 col-md-12 mb-5 d-flex justify-content-end mt-5">
-                    
-
-                    <div  class="min-vh100"style="max-width: 600px;">
-                        {{-- <h2 class="font-rounded mb-3 text-center">Benvenuto!</h2> --}}
-                        <p class="lead text-justify font-rounded mb-4 responsive-font" style="font-size: 20px;">
-                            Benvenuto nella nostra <strong>comunità giornalistica</strong>, dove ogni parola ha valore e
-                            ogni storia trova spazio.</p>
-                        <p class="lead text-justify font-rounded mb-4 responsive-font" style="font-size: 20px;">La tua
-                            voce è fondamentale qui. Scrivi articoli che incantano, stimolano e aprono nuovi orizzonti.
-                        </p>
-                        <p class="lead text-justify font-rounded mb-4 responsive-font" style="font-size: 20px;">Prenditi
-                            un momento per lasciarti trasportare dal potere delle parole mentre esplori, crei e
-                            condividi il tuo mondo con il nostro.</p>
-                        <p class="lead text-justify font-rounded mb-4 responsive-font" style="font-size: 20px;">Cosa
-                            stai aspettando? Crea un articolo adesso!</p>
-                    </div>
 
 
-                </div>
-
->>>>>>> 908cae85dbfe4b9fdabd735fc95a4d4272bc770a
             </div>
             <div class="col-lg-5 col-md-8 col-sm-10 m-3">
                 <img src="/images/article-careers-image2.png" alt="Descrizione dell'immagine" class="img-fluid card-3">
@@ -138,26 +85,4 @@
                     </form>
                 </div>
 
-<<<<<<< HEAD
-                <script>
-                    function showDescription(role) {
-                        var description = '';
-                        switch (role) {
-                            case 'admin':
-                                description =
-                                    'L\'amministratore ha il controllo totale sul blog, può creare, modificare e cancellare post.';
-                                break;
-                            case 'editor':
-                                description = 'Il redattore può creare e modificare post, ma non può cancellarli.';
-                                break;
-                            case 'reviewer':
-                                description = 'Il revisore può leggere e commentare i post, ma non può creare o modificare post.';
-                                break;
-                        }
-                        document.getElementById('roleDescription').innerText = description;
-                    }
-                </script>
-=======
-
->>>>>>> 908cae85dbfe4b9fdabd735fc95a4d4272bc770a
 </x-layout-careers>
